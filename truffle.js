c129bb23a953c43a4042d8696d454ab6841b4a89
ayman <enkhalifapro@gmail.com>
2018-03-15 13:07:59 +0200

    deploy

--- truffle.js
+++ truffle.js
@@ -3,7 +3,12 @@ module.exports = {
         development: {
             host: "localhost",
             port: 8545,
+            from: "0x1b740b2d7948ebcd48abe02d83edb2c7e96a4414",
             network_id: "*" // Match any network id
+        },
+        ropsten: {
+            provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
+            network_id: '3',
         }
     }
 };
